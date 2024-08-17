85adf71768ee3603a0b2ddc76308a94bbb3e2b11
Gianmarco Calbi <calbigianmarco@gmail.com>
2024-08-17 10:55:24 +0200

    feat: Add  and  methods to list extension

--- packages/flext_core/lib/src/list.dart
+++ packages/flext_core/lib/src/list.dart
@@ -1,13 +1,36 @@
+import 'dart:math';
+
+/// Extension methods for [List].
 extension FlextCoreList<T> on List<T> {
+  /// Adds [item] to the list if it is not `null`.
   void maybeAdd(T? item) {
     if (item != null) {
       add(item);
     }
   }
 
+  /// Adds all [items] that are not `null` the list.
   void addAllNotNull(Iterable<T?> items) {
     for (final item in items) {
       maybeAdd(item);
     }
+  }
+
+  /// Returns a random element from the list.
+  T pickOneRandom([Random? random]) =>
+      this[(random ?? Random()).nextInt(length)];
+
+  /// Returns a list of [amount] random elements from the list.
+  List<T> pickNRandom(int amount, [Random? random]) {
+    RangeError.checkValidIndex(
+      amount - 1,
+      this,
+      'number',
+      length,
+      'number must be greater than 0 and less than length',
+    );
+    return toList()
+      ..shuffle(random)
+      ..sublist(0, amount);
   }
 }
